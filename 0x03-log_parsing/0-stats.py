190caa610b2a72bd5bade7dec114a1fcbb04ab4f
DevMaxwell <89994817+DevMaxwell@users.noreply.github.com>
2024-01-18 11:18:08 +0000

    task: 0x03. Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,88 @@
 #!/usr/bin/python3
-"""
-Calculate the fewest number of copy and paste operations needed to achieve 'n' 'H' characters in a text file.
-
-:param n: The desired number of 'H' characters to achieve in the file.
-:type n: int
-:return: The minimum number of copy and paste operations required to achieve 'n' 'H' characters. If 'n' is impossible to achieve, return 0.
-:rtype: int
-"""
+'''A script for parsing HTTP request logs.
+'''
+import re
 
 
-def minOperations(n):
-    """
-    Calculate the fewest number of operations needed to result in exactly 'n' H characters in the file.
+def extract_input(input_line):
+    '''Extracts sections of a line of an HTTP request log.
+    '''
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
-    :param n: The desired number of 'H' characters in the file.
-    :type n: int
-    :return: The minimum number of operations required to achieve 'n' 'H' characters.
-    :rtype: int
 
-    If 'n' is impossible to achieve, return 0.
-    """
+def print_statistics(total_file_size, status_codes_stats):
+    '''Prints the accumulated statistics of the HTTP request log.
+    '''
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
-    if n <= 1:
-        return 0  # It's impossible to have fewer than 2 'H' characters.
 
-    # Initialize an array to store the minimum operations required for each number of 'H' characters.
-    operations = [0] * (n + 1)
+def update_metrics(line, total_file_size, status_codes_stats):
+    '''Updates the metrics from a given HTTP request log.
 
-    for i in range(2, n + 1):
-        # Initialize operations[i] to a maximum value initially.
-        operations[i] = float('inf')
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
 
-        # Try to find a factor j such that we can copy-paste 'j' characters to reach 'i'.
-        for j in range(1, i):
-            if i % j == 0:
-                # Copy 'j' characters and then paste (i // j - 1) times.
-                operations[i] = min(operations[i], operations[j] + (i // j))
+    Returns:
+        int: The new total file size.
+    '''
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
 
-    return operations[n]
 
-# Example usage:
-n = 9
-result = minOperations(n)
-print(f"Minimum operations to achieve {n} 'H' characters: {result}")
+def run():
+    '''Starts the log parser.
+    '''
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
+    try:
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(total_file_size, status_codes_stats)
+
+
+if __name__ == '__main__':
+    run()
